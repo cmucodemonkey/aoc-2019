876fdfddc8db63dd9f7b2e80d12226069a5fb801
Steven Ball <stevenball88@gmail.com>
2020-11-27 23:38:59 -0500

    Initial commit

---
+++ Day1.Part1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day1.Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> moduleWeights = new List<int>
+            {
+                85644
+                ,52584
+                ,72349
+                ,83834
+                ,56593
+                ,108492
+                ,94585
+                ,97733
+                ,62732
+                ,103113
+                ,133259
+                ,132647
+                ,52460
+                ,51299
+                ,115749
+                ,121047
+                ,69451
+                ,54737
+                ,62738
+                ,116686
+                ,57293
+                ,97273
+                ,128287
+                ,139440
+                ,97583
+                ,130263
+                ,79307
+                ,118198
+                ,82514
+                ,70679
+                ,64485
+                ,119346
+                ,136281
+                ,114724
+                ,73580
+                ,76314
+                ,126198
+                ,97635
+                ,114655
+                ,104195
+                ,99469
+                ,70251
+                ,82815
+                ,79531
+                ,58135
+                ,80625
+                ,73106
+                ,139806
+                ,138478
+                ,136605
+                ,111472
+                ,149915
+                ,95928
+                ,126905
+                ,70496
+                ,147999
+                ,148501
+                ,114025
+                ,75716
+                ,113473
+                ,95390
+                ,104466
+                ,138715
+                ,53053
+                ,79502
+                ,98601
+                ,115139
+                ,122315
+                ,88402
+                ,124332
+                ,140107
+                ,50912
+                ,104885
+                ,142005
+                ,145938
+                ,118556
+                ,101858
+                ,51142
+                ,94100
+                ,99421
+                ,84544
+                ,137234
+                ,126374
+                ,107333
+                ,82439
+                ,125373
+                ,51212
+                ,99358
+                ,82821
+                ,89913
+                ,67513
+                ,136907
+                ,133707
+                ,139988
+                ,96914
+                ,130672
+                ,66474
+                ,120729
+                ,50131
+                ,67475
+            };
+
+            int sum = 0;
+
+            foreach (int moduleWeight in moduleWeights)
+            {
+                int fuelRequired = CalculateRequiredFuel(moduleWeight);
+                sum += fuelRequired;
+            }
+
+            Console.WriteLine(sum);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+        }
+
+        private static int CalculateRequiredFuel(int weight)
+        {
+            return Convert.ToInt32(Math.Floor(Convert.ToDecimal(weight / 3)) - 2);
+        }
+    }
+}
---
+++ Day2.Part1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Day2.Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,13,19,2,9,19,23,1,23,6,27,1,13,27,31,1,31,10,35,1,9,35,39,1,39,9,43,2,6,43,47,1,47,5,51,2,10,51,55,1,6,55,59,2,13,59,63,2,13,63,67,1,6,67,71,1,71,5,75,2,75,6,79,1,5,79,83,1,83,6,87,2,10,87,91,1,9,91,95,1,6,95,99,1,99,6,103,2,103,9,107,2,107,10,111,1,5,111,115,1,115,6,119,2,6,119,123,1,10,123,127,1,127,5,131,1,131,2,135,1,135,5,0,99,2,0,14,0";
+            int[] memory = Array.ConvertAll(input.Split(','), int.Parse);
+
+            for (int index = 0; index < memory.Length; index += 4)
+            {
+                if (memory[index] == 1)
+                {
+                    int firstOperand = memory[index + 1];
+                    int secondOperand = memory[index + 2];
+                    int saveIndex = memory[index + 3];
+                    memory[saveIndex] = firstOperand + secondOperand;
+                }
+                else if (memory[index] == 2)
+                {
+                    int firstOperand = memory[index + 1];
+                    int secondOperand = memory[index + 2];
+                    int saveIndex = memory[index + 3];
+                    memory[saveIndex] = firstOperand * secondOperand;
+                }
+                else if (memory[index] == 99)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(memory[0]);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+        }
+    }
+}
